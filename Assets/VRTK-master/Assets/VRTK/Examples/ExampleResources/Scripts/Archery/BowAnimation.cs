bd06196393a4f7cbaed069e1ac2e30e40844939c
huyle333 <huyle333@bu.edu>
2018-08-09 17:08:16 -0700

    Downgraded OculusPlatform and VRTK.

---
+++ Assets/VRTK-master/Assets/VRTK/Examples/ExampleResources/Scripts/Archery/BowAnimation.cs
@@ -0,0 +1,16 @@
+﻿namespace VRTK.Examples.Archery
+{
+    using UnityEngine;
+
+    public class BowAnimation : MonoBehaviour
+    {
+        public Animation animationTimeline;
+
+        public void SetFrame(float frame)
+        {
+            animationTimeline["bowPull"].speed = 0;
+            animationTimeline["bowPull"].time = frame;
+            animationTimeline.Play("bowPull");
+        }
+    }
+}
