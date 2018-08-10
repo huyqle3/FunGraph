5c3fe4e9ad681fa07b024e4c1b72564ad9c38740
jarusified <jarus3001@gmail.com>
2018-08-10 11:06:57 -0700

    Maze- not correct but there

--- Assets/Scripts/HuntAndKill.cs
+++ Assets/Scripts/HuntAndKill.cs
@@ -4,55 +4,68 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HuntAndKill : AlgorithmClass{
-	public int currentRow = 0;
-	public int currentColumn = 0;
+	public int cRow = 0;
+	public int cColumn = 0;
 
-	private int maxRows, maxColumns;
+	public int maxRows = 16, maxColumns = 16;
 
 	private bool courseComplete = false;
 
 	public HuntAndKill(MazeCell[,] mazeCells) : base(mazeCells) {
 	}
+
+	public static int currentPosition = 0;
+	public const string key = "123424123342421432233144441212334432121223344";
+
+	public static int GetNextNumber() {
+		string currentNum = key.Substring(currentPosition++ % key.Length, 1);
+		return int.Parse (currentNum);
+	}
 	
 	public void run(){
-		maxRows = mazeCells.Length;
-		maxColumns = maxRows;
-		
 		//Set the [0,0] as visited 
-		mazeCells [currentRow, currentColumn].visited = true;
+		mazeCells [cRow, cColumn].visited = true;
 		while (! courseComplete) {			
-			Debug.Log('a');
 			kill();
-			Debug.Log('c');
 			hunt(); 
 		}
 	}
 
 	private void kill(){
-		while( isRoute(currentRow, currentColumn) ){
-			int direction = Mathf.FloorToInt(UnityEngine.Random.Range(0,4));
+		while( isRoute(cRow, cColumn) ){
+			int direction = GetNextNumber ();
+			Debug.Log(direction);
+			if (direction == 1 && isAvailable (cRow- 1, cColumn)) {
+				// North
+				destroyWall (mazeCells [cRow, cColumn].northWall);
+				destroyWall (mazeCells [cRow- 1, cColumn].southWall);
+				cRow--;
+			} else if (direction == 2 && isAvailable (cRow+ 1, cColumn)) {
+				// South
+				destroyWall (mazeCells [cRow, cColumn].southWall);
+				destroyWall (mazeCells [cRow+ 1, cColumn].northWall);
+				cRow++;
+			} else if (direction == 3 && isAvailable (cRow, cColumn + 1)) {
+				// east
+				destroyWall(mazeCells [cRow, cColumn].eastWall);
+				destroyWall (mazeCells [cRow, cColumn + 1].westWall);
+				cColumn++;
+			} else if (direction == 4 && isAvailable (cRow, cColumn - 1)) {
+				// west
+				destroyWall (mazeCells [cRow, cColumn].westWall);
+				destroyWall(mazeCells [cRow, cColumn - 1].eastWall);
+				cColumn--;
+			}
 
-			if( direction == 0 && mazeCells[currentRow - 1, currentColumn].visited ){
-				destroyWall( mazeCells[currentRow, currentColumn].northWall);
-				destroyWall( mazeCells[currentRow - 1, currentColumn].southWall);							 
-				currentRow--;
-			}
-			else if( direction == 1 && mazeCells[currentRow + 1, currentColumn].visited ){
-				destroyWall( mazeCells[currentRow, currentColumn].southWall);
-				destroyWall( mazeCells[currentRow + 1, currentColumn].northWall);
-				currentRow++;
-			}
-			else if( direction == 2 && mazeCells[currentRow, currentColumn - 1].visited ){
-				destroyWall( mazeCells[currentRow, currentColumn].eastWall);
-				destroyWall( mazeCells[currentRow, currentColumn - 1].westWall);
-				currentColumn--;
-			}
-			else if( direction == 3 && mazeCells[currentRow, currentColumn + 1].visited ){
-				destroyWall( mazeCells[currentRow, currentColumn].westWall);
-				destroyWall( mazeCells[currentRow, currentColumn + 1].eastWall);
-				currentColumn++;
-		    }
-			mazeCells[currentRow, currentColumn].visited = true;		
+			mazeCells[cRow, cColumn].visited = true;		
+		}
+	}
+
+	private bool isAvailable(int row, int column) {
+		if (row >= 0 && row < mazeRows && column >= 0 && column < mazeColumns && !mazeCells [row, column].visited) {
+			return true;
+		} else {
+			return false;
 		}
 	}
 
@@ -61,13 +74,12 @@ public class HuntAndKill : AlgorithmClass{
 		
 		for(int i = 0; i < maxRows; i++){
 			for(int j = 0; j < maxColumns; j++){
-				Debug.Log('b');
 				if (!mazeCells [i, j].visited && isRoute(i, j)) {
 					courseComplete = false;
-					currentRow = i;
-					currentColumn = j;
-					destroyAdjacentWall (currentRow, currentColumn);
-					mazeCells [currentRow, currentColumn].visited = true;
+					cRow = i;
+					cColumn = j;
+					destroyAdjacentWall (cRow, cColumn);
+					mazeCells [cRow, cColumn].visited = true;
 					//					return;
 				}
 			}
@@ -80,16 +92,15 @@ public class HuntAndKill : AlgorithmClass{
 		if(r > 0 && !mazeCells[r - 1, c].visited){
 			countVisited += 1;
 		}
-		else if(r < maxRows && !mazeCells[r + 1, c].visited){
+		else if(r < maxRows - 1 && !mazeCells[r + 1, c].visited){
 			countVisited += 1;
 		}
 		else if(c > 0 && !mazeCells[r, c - 1].visited){
 			countVisited += 1;
 		}
-		else if(c < maxColumns && !mazeCells[r, c + 1].visited){
+		else if(c < maxColumns - 1 && !mazeCells[r, c + 1].visited){
 			countVisited += 1;			
 		}
-
 		return countVisited > 0;
 	}
 
@@ -104,22 +115,22 @@ public class HuntAndKill : AlgorithmClass{
 
 		while(!wallDestroyed){
 			int direction = Mathf.FloorToInt(UnityEngine.Random.Range(0,4));
-			if(direction == 0 && r > 0 && mazeCells[r - 1, c].visited){
+			if(direction == 1 && r > 0 && mazeCells[r - 1, c].visited){
 				destroyWall(mazeCells[r, c].northWall);
 				destroyWall(mazeCells[r-1, c].southWall);
 				wallDestroyed = true;
 			}
-			else if(direction == 1 && r < maxRows - 2 && mazeCells[r + 1, c].visited){
+			else if(direction == 2 && r < maxRows - 2 && mazeCells[r + 1, c].visited){
 				destroyWall(mazeCells[r, c].southWall);
 				destroyWall(mazeCells[r+1, c].northWall);
 				wallDestroyed = true;
 			}
-			else if(direction == 2 && c > 0 && mazeCells[r, c - 1].visited){
+			else if(direction == 3 && c > 0 && mazeCells[r, c - 1].visited){
 				destroyWall(mazeCells[r, c].eastWall);
 				destroyWall(mazeCells[r, c-1].westWall);
 				wallDestroyed = true;
 			}
-			else if(direction == 3 && c < maxColumns - 2 && mazeCells[r, c + 1].visited){
+			else if(direction == 4 && c < maxColumns - 2 && mazeCells[r, c + 1].visited){
 				destroyWall(mazeCells[r, c].westWall);
 				destroyWall(mazeCells[r, c+1].eastWall);
 				wallDestroyed = true;
