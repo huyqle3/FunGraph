b19e56f84d4d337daa2aa6ec7508d517aa76a382
huyle333 <huyle333@bu.edu>
2018-08-10 08:51:53 -0700

    Added some dementor model and magic.

---
+++ Assets/Scripts/VRBasics/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    private void Start()
+    {
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/VRBasics/WandController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandController : MonoBehaviour {
+
+    GameObject spell;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+	}
+
+    // Expelliarmus
+    public void InitializeExpelliarmus()
+    {
+        if (!spell)
+        {
+            GameObject expelliarmusOrb = Instantiate(Resources.Load("Prefabs/Expelliarmus"), this.transform) as GameObject;
+            spell = expelliarmusOrb;
+
+            GameObject expelliarmusProjectile = expelliarmusOrb.transform.Find("ExpelliarmusProjectile").gameObject;
+
+            ExpelliarmusProjectile(expelliarmusProjectile);
+        }
+        else
+        {
+            CreateExpelliarmusProjectile();
+        }
+    }
+
+    public void CreateExpelliarmusProjectile()
+    {
+        GameObject expelliarmusProjectile = Instantiate(Resources.Load("Prefabs/ExpelliarmusProjectile"), spell.transform) as GameObject;
+        ExpelliarmusProjectile(expelliarmusProjectile);
+    }
+
+    public void ExpelliarmusProjectile(GameObject projectile)
+    {
+        Vector3 projectileDir = -projectile.transform.right;
+        Vector3 projectilePos = projectile.transform.position + projectileDir * 1.5f;
+
+        projectile.GetComponent<Rigidbody>().velocity = projectileDir * 10.0f;
+
+        Destroy(projectile, 8.0f);
+    }
+}
